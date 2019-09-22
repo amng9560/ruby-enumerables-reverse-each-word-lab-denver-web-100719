b4f8e6868df84e5d5b7c6e7d86fd58a1f3783fba
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-22 15:55:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
 def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+  reverse_words = sentence.split.collect {|word| word.reverse}.join(" ")
 end
